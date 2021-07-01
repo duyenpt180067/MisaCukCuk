5056b41a5fcfa0a548b8c9c185c5c328bb4a707d
DUYEN PHAM THI <ptduyen@rd.local>
2021-07-01 17:15:01 +0700

    update 7/1/2021

---
+++ js/employee.js
@@ -0,0 +1,14 @@
+var listemp = document.getElementById("employee");
+$('.btn-add-emp').click(function() {
+    $('.add-emp').addClass('open-add-emp');
+    listemp.style.filter = "opacity(0.1)";
+});
+$('.btn-close-add').click(function() {
+    $('.add-emp').removeClass('open-add-emp');
+    listemp.style.filter = "opacity(1)";
+});
+
+$('.findid input').on("focus", function() {
+    var findid = document.getElementById("findid");
+    findid.style.boxShadow = "2px 2px 20px rgb(0 0 0 / 40%)";
+})
